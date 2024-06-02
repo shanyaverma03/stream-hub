405fbf65bd2fbe3d031ea0a213397860441ed03e
Shanya <shanyaverma03@gmail.com>
2024-06-02 10:33:27 +0530

    Add routes to edit and delete destination

--- server/routes/destinationRoutes.js
+++ server/routes/destinationRoutes.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
 
-const { addDestination } = require("../controllers/destinationController");
+const {
+  addDestination,
+  editDestination,
+  deleteDestination,
+} = require("../controllers/destinationController");
 const { checkAuth } = require("../util/auth");
 
 router.use(checkAuth);
 
 router.post("/", addDestination);
+
+router.put("/", editDestination);
+
+router.delete("/", deleteDestination);
 
 module.exports = router;
