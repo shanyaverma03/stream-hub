414d5eba4ae17aa03faf6e02405b49eeaad404f2
Shanya <shanyaverma03@gmail.com>
2024-06-02 11:13:28 +0530

    Add edit and delete destination controllers

--- server/controllers/destinationController.js
+++ server/controllers/destinationController.js
@@ -13,7 +13,7 @@ module.exports.getDestinations = async (req, res, next) => {
 
 module.exports.addDestination = async (req, res) => {
   const { channel, apiKey } = req.body;
-  const hashedAPIKey = await bcrypt.hash(apiKey, 10);
+
   try {
     const { user } = req;
 
@@ -21,7 +21,7 @@ module.exports.addDestination = async (req, res) => {
       user._id,
       {
         $push: {
-          destinations: { channel, apiKey: hashedAPIKey },
+          destinations: { channel, apiKey },
         },
       },
       { new: true }
@@ -31,5 +31,54 @@ module.exports.addDestination = async (req, res) => {
   } catch (error) {
     console.error("Error adding destination", error);
     res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports.editDestination = async (req, res) => {
+  const { channel, apiKey } = req.body;
+
+  try {
+    const { user } = req;
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id, "destinations.channel": channel },
+      {
+        $set: {
+          "destinations.$.apiKey": apiKey,
+        },
+      },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Destination not found" });
+    }
+    res.status(200).json({ updatedDestinations: updatedUser.destinations });
+  } catch (error) {
+    console.error("Error editing destination", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports.deleteDestination = async (req, res) => {
+  const { channel } = req.query;
+
+  try {
+    const { user } = req;
+    const updatedUser = await User.findByIdAndUpdate(
+      user._id,
+      {
+        $pull: { destinations: { channel } },
+      },
+      { new: true }
+    );
+    if (updatedUser) {
+      res.status(200).json({ updatedDestinations: updatedUser.destinations });
+    } else {
+      res
+        .status(404)
+        .json({ message: "User not found or destination not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
   }
 };
